a074baeec5b6023540f9fb918da3f36e1c9ccffe
高廷旺 <code_butcher@sina.com>
2018-08-04 17:29:02 +0800

    使用xpath解析具体页面

--- GtwSpiders/spiders/jobbole.py
+++ GtwSpiders/spiders/jobbole.py
@@ -1,11 +1,41 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    # start_urls = ['http://blog.jobbole.com/']
+
+    # 爬取具体文章页举例
+    start_urls = ['http://blog.jobbole.com/110287/']
 
     def parse(self, response):
+        ### 提取文章的具体字段
+        # 标题
+        title = response.xpath('//div[@class="entry-header"]/h1/text()').extract_first("")
+        create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·","").strip()
+        # 点赞数
+        praise_nums = response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract()[0]
+
+        # 收藏数
+        fav_nums = response.xpath("//span[contains(@class, 'bookmark-btn')]/text()").extract()[0]
+        match_re = re.match(".*?(\d+).*", fav_nums)
+        if match_re:
+            fav_nums = match_re.group(1)
+
+        # 评论数
+        comment_nums = response.xpath("//a[@href='#article-comment']/span/text()").extract()[0]
+        match_re = re.match(".*?(\d+).*", comment_nums)
+        if match_re:
+            comment_nums = match_re.group(1)
+
+        # 正文内容
+        content = response.xpath("//div[@class='entry']").extract()[0]
+
+        tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        tags = ",".join(tag_list)
+
         pass
