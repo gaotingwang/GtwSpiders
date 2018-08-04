5c49212efa87fbfcf9384bc6042d5e32116a5a76
高廷旺 <code_butcher@sina.com>
2018-08-04 18:17:26 +0800

    使用css解析页面

--- GtwSpiders/spiders/jobbole.py
+++ GtwSpiders/spiders/jobbole.py
@@ -14,27 +14,33 @@ class JobboleSpider(scrapy.Spider):
     def parse(self, response):
         ### 提取文章的具体字段
         # 标题
-        title = response.xpath('//div[@class="entry-header"]/h1/text()').extract_first("")
-        create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·","").strip()
+        title = response.xpath('//div[@class="entry-header"]/h1/text()').extract_first("")  # xpath获取文本内容通过./text()
+        title = response.css(".entry-header h1::text").extract_first()  # css通过::text来获取文本内容
+
         # 点赞数
-        praise_nums = response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract()[0]
+        create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·","").strip()
 
         # 收藏数
-        fav_nums = response.xpath("//span[contains(@class, 'bookmark-btn')]/text()").extract()[0]
+        praise_nums = response.css(".vote-post-up h10::text").extract()[0]
+        fav_nums = response.css(".bookmark-btn::text").extract()[0]
         match_re = re.match(".*?(\d+).*", fav_nums)
         if match_re:
-            fav_nums = match_re.group(1)
+            fav_nums = int(match_re.group(1))
+        else:
+            fav_nums = 0
 
         # 评论数
-        comment_nums = response.xpath("//a[@href='#article-comment']/span/text()").extract()[0]
+        comment_nums = response.css("a[href='#article-comment'] span::text").extract()[0]
         match_re = re.match(".*?(\d+).*", comment_nums)
         if match_re:
-            comment_nums = match_re.group(1)
+            comment_nums = int(match_re.group(1))
+        else:
+            comment_nums = 0
 
         # 正文内容
-        content = response.xpath("//div[@class='entry']").extract()[0]
+        content = response.css("div.entry").extract()[0]
 
-        tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
         tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
         tags = ",".join(tag_list)
 
